f767df1cc3b299f36ac59ad524c282ea2cd608b9
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-03-14 13:02:32 -0500

    PrintingNamesIndex Stable - Pre ChatGBT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -2,3 +2,11 @@
 # Date: 3.14.2024
 # Program: Printing Name Index
 # resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ["Corey","Chris","travis","Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
+    

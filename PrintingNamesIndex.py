d0d71fed68f1d524aca2227ce715646f76267c6b
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-03-14 13:09:35 -0500

    Condensed Printing Names Index - stable - ChatGP

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -3,10 +3,8 @@
 # Program: Printing Name Index
 # resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
-names = ["Corey","Chris","travis","Dave"]
+names = ["Corey", "Chris", "travis", "Dave"]
 
-index = 0
-for name in names:
-    print(index,name)
-    index += 1
-    
+for index, name in enumerate(names):
+    print(index, name)
+
